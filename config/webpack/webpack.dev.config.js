8471a37d2c65dfc60e10038db57b61d1f045ca42
Liuyll <540374501@qq.com>
2020-01-19 16:04:41 +0800

    修复开发模式source-map

--- config/webpack/webpack.dev.config.js
+++ config/webpack/webpack.dev.config.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge')
 
 module.exports = merge(common,{
     mode: 'development',
+    devtool: 'inline-source-map',
     devServer: {
         contentBase: resolveApp('../../public'),
         port: 9000,
